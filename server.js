a583648c33d91602d913da54f7728d1bdcba5baa
CptSurfinBird <jacobgittings@gmail.com>
2017-01-30 02:57:50 -0500

    finished

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var connect = require('connect');
-var http = require('http');
-var bodyParser = require('body-parser');
+var url = require('url');
 
 
 var app = connect();
@@ -10,28 +9,30 @@ var server = app.listen(process.env.PORT||3000, '0.0.0.0', function () {
     console.log();
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 // API
-app.get('/lab2',function(req, res){
-	var method = request.query.method;
-	var x = request.query.x;
-	var y = request.query.y;
+app.use('/lab2',function(req, res, next){
+	var qs = url.parse(req.url, true).query;
+	var method = qs.method;
+	var x = qs.x;
+	var y = qs.y;
 
 	switch(method) {
     case "add":
-        res.send(x+'+'+y+'='(x+y));
+        res.end(x + '+' + y + '=' + (parseInt(x) + parseInt(y)));
         break;
     case "subtract":
-        res.send(x+'-'+y+'='(x-y));
+        res.end(x + '-' + y + '=' + (x - y));
         break;
     case "multiply":
-        res.send(x+'*'+y+'='(x*y));
+        res.end(x + '*' + y + '=' + (x * y));
 		break;
 	case "divide":
-        res.send(x+'/'+y+'='(x/y));
+        res.end(x + '/' + y + '=' + (x / y));
 		break;
 	default:
-		res.send("It seems we've taken a wrong turn.");
+		res.end("It seems we've taken a wrong turn.");
 	}
+
+	res.end('<h1>Calculator</h1>' + x + [method] + y)
 
 });
